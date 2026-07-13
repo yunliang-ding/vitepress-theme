8dc64e2ff5da3d961fd2160631434a1fc87d2aee
yunlaing-ding <11065047+yunling-ding@user.noreply.gitee.com>
2026-07-13 20:53:33 +0800

    0.0.8

--- react/layout.vue
+++ react/layout.vue
@@ -4,7 +4,7 @@ import { useData } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import Playground from "./playground.vue";
 import setTheme from "../color";
-import { watch, nextTick, provide, inject, onMounted } from "vue";
+import { watch, inject, onMounted } from "vue";
 import type { ThemeHooks } from "../index";
 // @ts-ignore
 import NiceDropdown from "../drop-down.vue";
@@ -58,40 +58,6 @@ const colorConfig = [
 
 const { frontmatter, isDark } = useData();
 
-const enableTransitions = () =>
-  "startViewTransition" in document &&
-  window.matchMedia("(prefers-reduced-motion: no-preference)").matches;
-
-provide("toggle-appearance", async ({ clientX: x, clientY: y }: MouseEvent) => {
-  if (!enableTransitions()) {
-    isDark.value = !isDark.value;
-    return;
-  }
-
-  const clipPath = [
-    `circle(0px at ${x}px ${y}px)`,
-    `circle(${Math.hypot(
-      Math.max(x, innerWidth - x),
-      Math.max(y, innerHeight - y)
-    )}px at ${x}px ${y}px)`,
-  ];
-  // @ts-ignore
-  await document.startViewTransition(async () => {
-    isDark.value = !isDark.value;
-    await nextTick();
-  }).ready;
-
-  document.documentElement.animate(
-    { clipPath: isDark.value ? clipPath.reverse() : clipPath },
-    {
-      duration: 300,
-      easing: "ease-in",
-      fill: "forwards",
-      pseudoElement: `::view-transition-${isDark.value ? "old" : "new"}(root)`,
-    }
-  );
-});
-
 onMounted(() => {
   watch(isDark, async (dark) => {
     const color = localStorage?.getItem("color") as any;
@@ -138,29 +104,3 @@ onMounted(() => {
     </template>
   </DefaultTheme.Layout>
 </template>
-
-<style>
-::view-transition-old(root),
-::view-transition-new(root) {
-  animation: none;
-  mix-blend-mode: normal;
-}
-
-::view-transition-old(root),
-.dark::view-transition-new(root) {
-  z-index: 1;
-}
-
-::view-transition-new(root),
-.dark::view-transition-old(root) {
-  z-index: 9999;
-}
-
-.VPSwitchAppearance {
-  width: 22px !important;
-}
-
-.VPSwitchAppearance .check {
-  transform: none !important;
-}
-</style>
